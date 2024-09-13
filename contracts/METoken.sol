fb7dc29e49379a567952b124f3c6d81717932b54
Yadneya Joshi <61934942+Yadnu@users.noreply.github.com>
2024-09-13 05:38:47 +0530

    Assignment

---
+++ contracts/METoken.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.10;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Metoken is ERC20 {
+    mapping(address => uint256) private balances;
+    string private constant _name = 'Mastering Ethereum Token';
+    string private constant _symbol = 'MET';
+    uint8 private constant _decimals = 2;
+    uint private constant _initial_supply = 2100000000;
+    constructor() ERC20(_name, _symbol){
+        balances[msg.sender] = _initial_supply;
+        emit Transfer(address(0), msg.sender, _initial_supply);
+    }
+}
